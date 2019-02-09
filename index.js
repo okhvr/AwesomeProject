77ef5f7d51b368a9b6cff4acbaa857ab71144403
Olha Katsalap <olg.katsalap@gmail.com>
2019-02-09 11:20:09 +0200

    madd navigation tabs

--- index.js
+++ index.js
@@ -3,8 +3,20 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
+import React from "react";
+import {AppRegistry, View, Text} from 'react-native';
+import { createAppContainer, createBottomTabNavigator } from "react-navigation";
+
+import FeedScreen from './src/screens/feed/FeedScreen';
+import FirstScreen from './src/screens/first/FirstScreen';
+import ThirdScreen from './src/screens/third/ThirdScreen';
+
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+const TabNavigator = createBottomTabNavigator({
+    Feed: FeedScreen,
+    First: FirstScreen,
+    Third: ThirdScreen
+  });
+
+AppRegistry.registerComponent(appName, () => createAppContainer(TabNavigator));
